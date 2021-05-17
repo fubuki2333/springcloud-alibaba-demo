2328583038fc7c03abda7f8ca01d17ba3d98a12f
fubuki2333 <gaochenyu2012@126.com>
2021-05-17 22:41:56 +0800

    添加Sleuth链路追踪

--- cloud-provider-payment8001/src/main/java/com/chenyu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/chenyu/springcloud/controller/PaymentController.java
@@ -88,4 +88,9 @@ public class PaymentController {
         }
         return serverPort;
     }
+
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin() {
+        return "hi,I am payment_zipkin server fall back, welcome to my world!";
+    }
 }
